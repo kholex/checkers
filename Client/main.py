879955c65fec6bdf959aac787317f4ebea7a7ddc
Alexander <alekholodov@ozon.ru>
2023-04-14 22:48:59 +0300

    Реализовал на клиенте отрисовку шахматной доски

---
+++ Client/main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from Field import Field
+
+cell_size = 60
+field_size = cell_size * 8
+
+if __name__ == '__main__':
+    screen = tk.Tk()
+
+    canvas = tk.Canvas(screen, width=field_size, height=field_size)
+    canvas.pack()
+
+    field = Field(canvas, cell_size)
+    field._draw_cells()
+    field._init_checkers()
+
+    screen.title('Checkers')
+    screen.resizable(0, 0)
+    screen.mainloop()
